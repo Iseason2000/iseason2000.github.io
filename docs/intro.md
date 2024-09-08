059dafb55e6abe664aac8dfdc02e1c562c62b522
Iseason <1347811744@qq.com>
2024-09-08 21:49:14 +0800

    init

--- docs/intro.md
+++ docs/intro.md
@@ -2,46 +2,17 @@
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# 导览
 
-Let's discover **Docusaurus in less than 5 minutes**.
-
-## Getting Started
-
-Get started by **creating a new site**.
-
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
-
-### What you'll need
-
-- [Node.js](https://nodejs.org/en/download/) version 18.0 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
-
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**.
-
-The classic template will automatically be added to your project after you run the command:
-
-```bash
-npm init docusaurus@latest my-website classic
-```
-
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
-
-The command also installs all necessary dependencies you need to run Docusaurus.
-
-## Start your site
-
-Run the development server:
-
-```bash
-cd my-website
-npm run start
-```
-
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
-
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
-
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+## Minecraft 插件
+:::tip[tip] 
+插件交流群: 887296263 [点击加入群聊](https://qm.qq.com/q/6d1zREKbOo)
+:::
+ * 【绑定】SakuraBind: https://github.com/SakuraTown/SakuraBind
+ * 【MMOItems附属】MMOForge: https://github.com/SakuraTown/MMOForge
+ * 【支付中台】SakuraPurchase: https://github.com/Iseason2000/SakuraPurchase
+ * 【CDK兑换】SakuraCDK: https://github.com/SakuraTown/SakuraCDK
+ * 【物品封印】ItemSeals: https://github.com/Iseason2000/ItemSeals
+ * 【系统邮件】SakuraMail: https://github.com/SakuraTown/SakuraMail (停止维护)
+ * 【附魔】DeEnchantment: https://github.com/Iseason2000/DeEnchantment (停止维护)
+ * 【插件限制】PluginLimiter: https://github.com/Iseason2000/PluginLimiter (停止维护)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Iseason\'s documentation',
+  tagline: 'there is the season under sea',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -72,23 +72,27 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: '首页',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: '导览',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            label: '博客',
+            href: 'https://www.iseason.top',
+            position: 'left'
+          },
+          {
+            href: 'https://github.com/Iseason2000',
             label: 'GitHub',
             position: 'right',
           },
@@ -97,29 +101,29 @@ const config = {
       footer: {
         style: 'dark',
         links: [
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
           {
-            title: 'Docs',
+            title: '社交',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'QQ',
+                href: 'tencent://message/?uin=1347811744',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'Github',
+                href: 'https://github.com/Iseason2000',
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: 'BiliBili',
+                href: 'https://space.bilibili.com/8689588',
               },
             ],
           },
@@ -127,8 +131,8 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: '博客',
+                href: 'https://www.iseason.top',
               },
               {
                 label: 'GitHub',
@@ -137,7 +141,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} by Iseason, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
